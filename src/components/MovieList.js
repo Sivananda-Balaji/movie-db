1027d683779bf4e1df61fcb8e29f0d3e677c6f29
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-07-15 03:09:56 +0530

    add fan-recommended movies page

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,41 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import MovieListItem from "./MovieListItem";
 
+const gridStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+};
+
 const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  const getPopularMovies = async () => {
+    try {
+      const options = {
+        method: "GET",
+        url: "https://api.themoviedb.org/3/movie/popular",
+        params: { language: "en-US", page: "1" },
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOTUzNWEzZTFhODJlNmZjYmQ1N2QyZDYyOGE4ZDA3NyIsInN1YiI6IjY0YWZlNGI1ZDY1OTBiMDBjN2FkZGEyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZdmG0BiTry6qm4QEszSkyDfLvWhPJhEdMxumbT5N8Do",
+        },
+      };
+
+      const result = await axios.request(options);
+      setMovies(result.data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getPopularMovies();
+  }, []);
+
   return (
-    <div>
-      <MovieListItem />
+    <div style={gridStyle}>
+      <MovieListItem movies={movies} />
     </div>
   );
 };
--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -1,9 +1,70 @@
 import React from "react";
+import {
+  Typography,
+  Grid,
+  Paper,
+  Divider,
+  useMediaQuery,
+  useTheme,
+  Box,
+} from "@mui/material";
 
-const MovieListItem = () => {
+const titleStyle = {
+  textAlign: "center",
+  margin: "1rem",
+};
+
+const gridItemStyle = {
+  boxShadow: "none",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  textAlign: "center",
+  paddingTop: "1rem",
+};
+
+const divderStyle = {
+  marginTop: "1rem",
+  width: "100%",
+};
+const MovieListItem = ({ movies }) => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
   return (
     <>
-      <p>Movie List Items</p>
+      <Typography variant="h3" component="h1" sx={titleStyle}>
+        What's Hot 🔥
+      </Typography>
+      <Box
+        sx={{
+          width: isSmallScreen ? "88%" : "75%",
+          marginBottom: "1rem",
+        }}
+      >
+        <Grid container spacing={2}>
+          {movies.map((movie) => {
+            return (
+              <Grid item xs={6} sm={4} md={3} key={movie.id}>
+                <Paper sx={gridItemStyle}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
+                    alt={movie.title}
+                    width="175rem"
+                    align="center"
+                  />
+                  <Typography variant="body1" component="p">
+                    {movie.vote_average} ⭐
+                  </Typography>
+                  <Typography variant="body1" component="p">
+                    {movie.title}
+                  </Typography>
+                  <Divider sx={divderStyle} />
+                </Paper>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Box>
     </>
   );
 };
