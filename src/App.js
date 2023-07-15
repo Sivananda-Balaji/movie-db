7c313963977133b3381294c8da551b13e8c6dcee
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-07-15 23:03:50 +0530

    import react for webpack configurations

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import MovieHeader from "./components/MovieHeader";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MovieList from "./components/MovieList";
